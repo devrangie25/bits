46359cc44da12b9098aaa265c9394489e758df19
devrangie25 <devrangie25@gmail.com>
2022-03-27 15:08:03 +0800

    add build release

---
+++ dist/_nuxt/5f14ab8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{656:function(n,e,t){"use strict";t.r(e);var l={layout:"default"},c=t(54),component=Object(c.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  Admin Panel\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
