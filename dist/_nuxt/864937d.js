5ce5d095dc9fd4fe85189df7694ae14a1043fe79
devrangie25 <devrangie25@gmail.com>
2022-03-28 12:53:04 +0800

    Build 2

--- dist/_nuxt/864937d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{657:function(n,e,t){"use strict";t.r(e);var l={layout:"default"},r=t(54),component=Object(r.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n    This is for public user\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
